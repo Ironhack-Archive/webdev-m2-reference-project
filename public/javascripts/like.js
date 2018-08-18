c9b5ec24a556fdb840374fd6f16b558aea9617b3
sebachowa <sebastian.nugnez@gmail.com>
2018-08-18 18:10:06 +0200

    added quote like toggle

--- public/javascripts/like.js
+++ public/javascripts/like.js
@@ -1,24 +1,30 @@
 'use strict';
 
-const heart = document.querySelectorAll('.heart');
+const main = () => {
+  const heart = document.querySelectorAll('.heart');
+  
+  heart.forEach((heart) => {
+    const handleClickEvent = (event) => {
+      const id = event.target.getAttribute('data');
+  
+      axios.post(`http://localhost:3000/quotes/${id}/like`)
+        .then(response => {
+          const likeCounterElement = event.path[3].childNodes[3].children[0].childNodes[0];
+  
+          if (response.data.action === 'liked') {
+            likeCounterElement.innerText = response.data.likes;
+            event.target.src = "images/liked.png";
+          } else if (response.data.action === 'unliked') {
+            event.target.src = "images/unliked.png";
+            likeCounterElement.innerText = response.data.likes;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+    heart.addEventListener('click', handleClickEvent);
+  });
+}
 
-heart.forEach((heart) => {
-  const handleClickEvent = (event) => {
-    const id = event.target.getAttribute('data');
-
-    axios.post(`http://localhost:3000/quotes/${id}/like`)
-      .then(response => {
-        const likeCounterElement = event.path[3].childNodes[3].children[0].childNodes[0];
-
-        if (response.data.action === 'liked') {
-          likeCounterElement.innerText = response.data.likes;
-        } else if (response.data.action === 'unliked') {
-          likeCounterElement.innerText = response.data.likes;
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  heart.addEventListener('click', handleClickEvent);
-});
+window.addEventListener("load", main)
